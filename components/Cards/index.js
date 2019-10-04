262dd935806d8eeb0bb46b6234952714e84cf25c
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-04 12:59:55 -0500

    done w cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,61 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// make function and grab grab
+
+
+
+function cardFabricator(item) {
+    //Elements
+    const keeperOfTheCards = document.querySelector(".cards-container"),
+    aCardThatIsKept = document.createElement("div"),
+      keptCardHeadline = document.createElement("div"),
+      keptCardAuthorContainer = document.createElement("div"),
+      keptCardImageContainer = document.createElement("div"),
+      cardImage = document.createElement("img"),
+      author = document.createElement("span");
+  
+    //Classes
+    aCardThatIsKept.classList.add("card");
+    keptCardHeadline.classList.add("headline");
+    keptCardAuthorContainer.classList.add("author");
+    keptCardImageContainer.classList.add("img-container");
+  
+    //TextContent
+    keptCardHeadline.textContent = item.headline;
+    cardImage.src = item.authorPhoto;
+    author.textContent = item.authorName;
+  
+    //Append 
+    keeperOfTheCards.appendChild(aCardThatIsKept);
+    aCardThatIsKept.appendChild(keptCardHeadline);
+    aCardThatIsKept.appendChild(keptCardAuthorContainer);
+    keptCardAuthorContainer.appendChild(keptCardImageContainer);
+    keptCardImageContainer.appendChild(cardImage);
+    keptCardAuthorContainer.appendChild(author);
+  
+    return aCardThatIsKept;
+  }
+  
+  const articleAxios = axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+  
+      const contents = Object.values(response.data.articles);
+  
+      contents.forEach(keys => {
+        keys.forEach(card => {
+          cardFabricator(card);
+        });
+      });
+    })
+    .catch(error => {
+      return error;
+    })
+
+
+
+
+
+
